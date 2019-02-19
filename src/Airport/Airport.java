596a1441e8d979acfc95029aad1644bfd7cbff46
Schurk, Fabian <fabian.schurk@web.de>
2019-02-19 15:02:50 +0100

    ss

--- src/Airport/Airport.java
+++ src/Airport/Airport.java
@@ -2,6 +2,15 @@ package Airport;
 
 public class Airport {
     public String name;
-    public Tower tower;
-    public ApronControl apronControl;
+    public Tower tower = new Tower();
+    private ApronControl apronControl = new ApronControl();
+    public Gate[] gate;
+
+    public Airport(String name) {
+        this.name = name;
+
+        for(int i = 0; i<10; i++){
+            gate[i] = new Gate();
+        }
+    }
 }
